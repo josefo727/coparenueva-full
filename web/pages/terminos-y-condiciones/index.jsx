986e462b8a3ae32c55bc651df85f1f1291dd1405
José R. Gutierrez <josefo727@gmail.com>
2023-03-16 16:04:11 -0500

    Feat: Terms and Conditions change text.

--- web/pages/terminos-y-condiciones/index.jsx
+++ web/pages/terminos-y-condiciones/index.jsx
@@ -41,13 +41,15 @@ export default function TermsAndConditions() {
                     <div className={styles.terms}>
                         <p><strong>Términos y condiciones para el cálculo del índice de persistencia:</strong></p>
                         <br/>
+
                         <span className={styles.separator}>Si el cliente tiene dos o más pólizas y/o contratos que se encuentren en el grupo objetivo de la campaña, cada uno será considerado de manera independiente para el cálculo del índice de persistencia.</span>
                         <span className={styles.separator}>Los cambios de plan/póliza/contrato en la misma compañía o entre compañías de clientes vigentes, serán considerados para el cálculo del índice de persistencia siempre que la vigencia del nuevo plan/póliza/contrato no exceda 31 días en relación con la cancelación del plan/póliza/contrato anterior.</span>
                         <span className={styles.separator}>Los cambios de Titular a Dependiente serán considerados para el cálculo del índice de persistencia, siempre que no vaya a una póliza/contrato que sea considerado nuevo negocio, es decir, que exceda los 31 días después de la fecha de su emisión.</span>
                         <span className={styles.separator}>Los cambios de Dependiente a Titular no serán considerados para el cálculo del índice de persistencia; puesto que el cálculo de dicho índice se hace por póliza, más no por afiliado/asegurado y son considerados como nuevo negocio.</span>
                         <span className={styles.separator}>La internacionalización de una póliza, es decir, el cambio de una póliza emitida en BMI Ecuador a BMI Miami, no sumará al cálculo del índice de persistencia.</span>
-                        <span className={styles.separator}>Los cambios de socios estratégicos no afectarán al cálculo de persistencia.</span>
-                        <span className={styles.separator}>Los cambios de agente no afectarán al cálculo de la persistencia del socio estratégico anterior y tampoco son parte del cálculo de persistencia del nuevo socio estratégico.</span>
+                        <span className={styles.separator}>Los cambios de socio estratégico no afectarán al cálculo de la persistencia del socio anterior, siempre y cuando la póliza y/o contrato se mantenga vigente hasta el 31 de diciembre del año de análisis. Y tampoco son parte del cálculo de persistencia del nuevo socio.</span>
+                        <span className={styles.separator}>Los casos de cancelación por fallecimiento se revisan de forma manual.</span>
+                        
                         <br/>
                         <p><strong>No se realizará ningún tipo de excepción que no esté explícitamente descrita en los términos y condiciones de la campaña.</strong></p>
                     </div>
