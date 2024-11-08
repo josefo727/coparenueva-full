5d4c289846a50d9511a9458b24b5402c034e9b10
José R. Gutierrez <josefo727@gmail.com>
2024-11-08 14:35:02 -0500

    fix: corrected user seeder

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     {
         \App\Models\User::factory(10)->create();
 
-        \App\Models\User::query()->create([
+        \App\Models\User::factory()->create([
             'name' => 'José R. Gutierrez',
             'email' => 'josefo727@gmail.com',
             'password' => bcrypt('000000'),
